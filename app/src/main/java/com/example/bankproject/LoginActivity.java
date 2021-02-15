5badf064c437c602c2e5475a63f4dd1263972c6e
mdsaddam1994hossain <saddam30061994@gmail.com>
2021-02-15 19:06:10 +0600

    myDbHelper add

--- app/src/main/java/com/example/bankproject/LoginActivity.java
+++ app/src/main/java/com/example/bankproject/LoginActivity.java
@@ -2,13 +2,35 @@ package com.example.bankproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+    EditText email,password;
+    Button loginButton,regButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        email = findViewById(R.id.email_id);
+        password = findViewById(R.id.password_id);
+        loginButton = findViewById(R.id.login_Button_Id);
+        regButton = findViewById(R.id.reg_Button_Id);
+
+        loginButton.setOnClickListener(this);
+        regButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId() == R.id.reg_Button_Id){
+            Intent intent =new Intent(this, MainActivity.class);
+            startActivity(intent);
+        }
     }
 }
---
+++ app/src/main/java/com/example/bankproject/MyDbHelper.java
@@ -0,0 +1,10 @@
+package com.example.bankproject;
+
+public class MyDbHelper {
+    static  final String DB_NAME = "onlineBank";
+    static final int VERSION =1;
+    static final String TABLE_NAME = "customer";
+    static final String CREATE_TABLE = "create table if not exists "+TABLE_NAME +" ";
+
+
+}
